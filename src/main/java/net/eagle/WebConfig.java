24a52be99d8a3f84e759405c44c37cc56b535a0a
Shawn <lishuai_247@163.com>
2018-01-18 21:50:38 +0800

    init

---
+++ src/main/java/net/eagle/WebConfig.java
@@ -0,0 +1,19 @@
+package net.eagle;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by Shawn on 2017/7/11.
+ */
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
